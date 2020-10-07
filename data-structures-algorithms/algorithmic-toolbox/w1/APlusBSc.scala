d6a64390147a17cca5ca8316cc0deee4c2da128a
kupal <9090852@gmail.com>
2020-10-08 01:54:22 +0300

    algo - 1.1 - A plus B.

---
+++ data-structures-algorithms/algorithmic-toolbox/w1/APlusBSc.scala
@@ -0,0 +1,10 @@
+package w1
+
+object APlusBSc extends App {
+  override def main(args: Array[String]): Unit = {
+    val scanner = new java.util.Scanner(System.in)
+    val line = scanner.nextLine()
+    val result = (line.split(" ").map(x => BigInt(x)) take 2).sum
+    System.out.print(result)
+  }
+}
